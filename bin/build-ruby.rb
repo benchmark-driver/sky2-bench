f5ac5f2ef6f1218364b6d35473668a685d4473ea
Takashi Kokubun <takashikkbn@gmail.com>
2019-05-13 23:58:40 +0900

    Change directory globally

--- bin/build-ruby.rb
+++ bin/build-ruby.rb
@@ -7,9 +7,11 @@
 build_ruby_revisions = Integer(ENV.fetch('BUILD_RUBY_REVISIONS', '1000'))
 build_ruby_repository = ENV.fetch('BUILD_RUBY_REPOSITORY')
 
-# older first
-latest_revisions = IO.popen(
-  ['git', 'log', '--pretty=format:%h', '--abbrev=10', '--reverse', '--topo-order', '-n', build_ruby_revisions.to_s], &:read)
-  .lines.map(&:strip)
+Dir.chdir(build_ruby_repository) do
+  # older first
+  latest_revisions = IO.popen(
+    ['git', 'log', '--pretty=format:%h', '--abbrev=10', '--reverse', '--topo-order', '-n', build_ruby_revisions.to_s], &:read)
+    .lines.map(&:strip)
 
-system('git', 'show', latest_revisions.first)
+  system('git', 'show', latest_revisions.first)
+end
