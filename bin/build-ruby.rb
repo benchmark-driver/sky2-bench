c8ebb0802b67652af74386288ff67377eb183014
Takashi Kokubun <takashikkbn@gmail.com>
2019-05-14 00:43:52 +0900

    Implement initial ruby builder

--- bin/build-ruby.rb
+++ bin/build-ruby.rb
@@ -1,7 +1,9 @@
 #!/usr/bin/env ruby
 # Executed by bin/run
+require 'shellwords'
 
 # Arguments:
+#   $BUILD_RUBY_BRANCH
 #   $BUILD_RUBY_REVISIONS
 #   $BUILD_RUBY_REPOSITORY
 #   $BUILD_RUBY_PREFIXES_DIR
@@ -10,28 +12,57 @@ build_ruby_repository = ENV.fetch('BUILD_RUBY_REPOSITORY')
 
 RubyBuilder = Module.new
 class << RubyBuilder
+  def branch
+    @branch ||= ENV.fetch('BUILD_RUBY_BRANCH')
+  end
+
   def prefixes_dir
     @prefixes_dir ||= ENV.fetch('BUILD_RUBY_PREFIXES_DIR')
   end
 
   # Assumes build_ruby_repository is checked out
   def install_revision(revision)
+    prefix = File.join(prefixes_dir, revision)
+    execute('git', 'reset', '--hard', revision) && (try_make || clean_make)
   end
 
   def uninstall_revision(revision)
-    system('rm', '-rf', File.join(prefixes_dir, revision), exception: true)
+    execute('rm', '-rf', File.join(prefixes_dir, revision), exception: true)
+  end
+
+  # #system with logs.
+  def execute(*args, exception: false)
+    puts "+ #{args.shelljoin}"
+    system(*args, exception: exception)
+  end
+
+  private
+
+  def try_make
+    execute("./configure --prefix=#{prefix.shellescape} --disable-install-doc") && execute('make', '-j4', 'all', 'install')
+  end
+
+  def clean_make
+    execute('make', 'clean')
+    execute('autoconf')
+    try_make
   end
 end
 
 Dir.chdir(build_ruby_repository) do
+  RubyBuilder.execute('git', 'fetch', 'origin', RubyBuilder.branch, exception: true)
+  RubyBuilder.execute('git', 'reset', '--hard', "remotes/origin/#{RubyBuilder.branch}", exception: true)
+
   latest_revisions = IO.popen(
     ['git', 'log', '--pretty=format:%h', '--abbrev=10', '--reverse', '--topo-order', '-n', build_ruby_revisions.to_s], &:read)
     .lines.map(&:strip) # older first
   built_revisions = Dir.glob(File.join(RubyBuilder.prefixes_dir, '*')).map(&File.method(:basename)).select { |v| v.match(/\A\h{10}\z/) }
 
   # GC obsoleted revisions
-  (built_revisions - latest_revisions).each do |obsolete_revision|
-    RubyBuilder.uninstall_revision(obsolete_revision)
+  if built_revisions.size > build_ruby_revisions
+    (built_revisions - latest_revisions).each do |obsolete_revision|
+      RubyBuilder.uninstall_revision(obsolete_revision)
+    end
   end
 
   latest_built_revision = latest_revisions.reverse.find { |rev| built_revisions.include?(rev) }
@@ -44,7 +75,6 @@ Dir.chdir(build_ruby_repository) do
 
   revisions_to_build.each do |revision|
     RubyBuilder.install_revision(revision)
-    puts "Build!: #{revision}"
-    break
+    break # DEBUG
   end
 end
