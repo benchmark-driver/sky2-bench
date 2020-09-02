19571bd819dbaf459acbad02caf99ab104a61c96
Takashi Kokubun <takashikkbn@gmail.com>
2020-09-02 16:22:48 -0700

    Make it the larger the better

--- benchmark/rubykon-benchmark.rb
+++ benchmark/rubykon-benchmark.rb
@@ -1,9 +1,10 @@
 require_relative 'rubykon/lib/rubykon'
 
+iterations = 1_000
 t = Time.now
 
 game_state_19 = Rubykon::GameState.new Rubykon::Game.new(19)
 mcts = MCTS::MCTS.new
-mcts.start game_state_19, 1_000
+mcts.start game_state_19, iterations
 
-p Time.now - t
+p(iterations / (Time.now - t))
