38e89fedbadc1f00c2543f47911a8d30000111e0
Takashi Kokubun <takashikkbn@gmail.com>
2021-11-10 09:40:24 -0800

    Resurrect method benchmarks

--- bin/commit-bench.rb
+++ bin/commit-bench.rb
@@ -24,9 +24,9 @@ BenchConfig = Struct.new(
 # vm_count, jit_count, yjit_count: --repeat-count
 pattern_configs = {
   'optcarrot/benchmark.yml'                    => BenchConfig.new(revisions: 4, vm_count: 4, jit_count: 4, yjit_count: 4, timeout:  60),
-  'optcarrot/benchmark_3000.yml'               => BenchConfig.new(revisions: 4, vm_count: 2, jit_count: 2, yjit_count: 2, timeout: 360),
+  'optcarrot/benchmark_3000.yml'               => BenchConfig.new(revisions: 2, vm_count: 2, jit_count: 2, yjit_count: 2, timeout: 360),
   'rubykon-benchmark.yml'                      => BenchConfig.new(revisions: 1, vm_count: 1, jit_count: 1, yjit_count: 1, timeout:  60),
-  'ruby-method-benchmarks/benchmarks/**/*.yml' => BenchConfig.new(revisions: 0, vm_count: 1, jit_count: 0, yjit_count: 0, timeout:  60),
+  'ruby-method-benchmarks/benchmarks/**/*.yml' => BenchConfig.new(revisions: 1, vm_count: 1, jit_count: 0, yjit_count: 0, timeout:  60),
 }
 
 ruby_revisions = Dir.glob(File.join(prefixes_dir, '*')).map(&File.method(:basename)).select { |f| f.match(/\A\h{10}\z/) }
